4ce9ad08f481c4905cc8e0c86c0aac9de8036893
Sergei <kvakazuabr@yandex.ru>
2022-10-17 23:42:01 +0300

    fixed remark

--- dropduwn.js
+++ dropduwn.js
@@ -1,37 +1,42 @@
 const list = document.querySelector(".trial_list");
 const elem = document.querySelector(".trial_nested-list");
 
-elem.addEventListener('click', (evt) => {
-  [...elem.children].forEach(el => el.classList.remove('show-active'))
-  evt.target.parentNode.classList.add('show-active')
-  document.querySelector('.trial_description-title').textContent = evt.target.textContent
-})
+elem.addEventListener("click", (evt) => {
+  if (evt.target instanceof HTMLParagraphElement) {
+    [...elem.children].forEach((el) => el.classList.remove("show-active"));
+    evt.target.parentNode.classList.add("show-active");
+    document.querySelector(".trial_description-title").textContent =
+      evt.target.textContent;
+  }
+});
 
 list.addEventListener("click", (evt) => {
   if (evt.target.classList.contains("trial_title")) {
     evt.target.classList.toggle("active");
     const nextElem = evt.target.nextElementSibling;
     nextElem.classList.toggle("visually-hidden");
-    [...document.querySelectorAll('.trial_nested-list')].filter((el) => {
+    [...document.querySelectorAll(".trial_nested-list")].map((el) => {
       if (el !== nextElem) {
         el.classList.add("visually-hidden");
       }
     });
 
-    [...document.querySelectorAll(".trial_title")].filter((el) => {
+    [...document.querySelectorAll(".trial_title")].map((el) => {
       if (el !== evt.target) {
         el.classList.remove("active");
       }
     });
 
-    nextElem?.addEventListener('click', (evt) => {
-      [...nextElem.children].forEach(el => el.classList.remove('show-active'))
-      evt.target.parentNode.classList.add('show-active')
-      document.querySelector('.trial_description-title').textContent = evt.target.textContent
-    })
+    nextElem?.addEventListener("click", (evt) => {
+      if (evt.target instanceof HTMLParagraphElement) {
+        [...nextElem.children].forEach((el) =>
+          el.classList.remove("show-active")
+        );
+
+        evt.target.parentNode.classList.add("show-active");
+        document.querySelector(".trial_description-title").textContent =
+          evt.target.textContent;
+      }
+    });
   }
 });
-
-
-
-
